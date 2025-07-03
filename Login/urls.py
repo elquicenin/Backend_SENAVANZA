9fe3c8353099649fc4d1e147825e25860501191e
TheBullst <jmontillavanegas@gmail.com>
2025-07-03 17:12:09 -0500

    arreglo

--- Login/urls.py
+++ Login/urls.py
@@ -1,16 +1,12 @@
 from django .urls import path
-from .api import login_admin, login_empresa, logout, verify ,CustomtokenObtainPairView
-from rest_framework_simplejwt import views as jwt_views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from .api import login_admin, login_empresa, logout ,CustomtokenObtainPairView, CustomTokenRefreshView
 
 
 urlpatterns = [
     # el path que precede a estas rutas es login/
     path('loginAdmin/', login_admin, name='login_admin'),
     path('loginEmpresa/', login_empresa, name='login_empresa'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', logout, name='logout'),
-    path('verify/', verify, name='verify'),
+    path('token/', CustomtokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
