4aaa026c94a82e5050ead1c04c3028590d4c53f5
elquicenin <quicenodavid29@gmail.com>
2025-10-02 00:34:06 -0500

    se cambio archivos el secret key

--- SENAVANZA2/settings.py
+++ SENAVANZA2/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR , '.env'))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-&mgomqc%wu)yp3m$b)8zmgazeg$b*vu7b0uzh$c8y2__6d4naf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
